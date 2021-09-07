196ca21398dee4ba55a56f40bee8b60c931611fb
Riley <riley@rileysmith.me>
2021-09-07 16:30:48 -0700

    make card be full height at load.

--- src/components/CodeWriter.tsx
+++ src/components/CodeWriter.tsx
@@ -88,7 +88,7 @@ export default IndexPage;`;
 	}, [ containerElement.current?.scrollHeight ]);
 
 	return (
-		<Card variant="outlined" sx={{ maxHeight: "340px", overflowY: "hidden", width: { sm: 1 }  }} ref={containerElement}>
+		<Card variant="outlined" sx={{ height: "340px", overflowY: "hidden", width: { sm: 1 }, background: "rgb(40, 44, 52) none repeat scroll 0% 0%"  }} ref={containerElement}>
 			<ReactSyntaxHighlighter language="javascript" style={atomOneDark} customStyle={{ marginTop: "0px", marginBottom: "0px", overflowX: "hidden !important" }} wrapLines>
 				{runningText}
 			</ReactSyntaxHighlighter>
