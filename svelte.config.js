2adfd92ea719b91c76f271b45a9203e9d714f4d7
Riley Smith <riley@rileysmith.me>
2024-08-05 19:21:58 -0700

    update deps, move to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 import seqPreprocessor from 'svelte-sequential-preprocessor'
-import { preprocessThrelte } from '@threlte/preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: seqPreprocessor([preprocess({ postcss: true }), preprocessThrelte()]),
+	preprocess: seqPreprocessor([preprocess({ postcss: true })]),
 
 	kit: {
-		adapter: adapter(),
-		alias: {
-		}
+		adapter: adapter({
+			edge: false,
+			split: false,
+		})
 	}
 };
 
