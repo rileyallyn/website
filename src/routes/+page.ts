31f3cf28f99796b6294214841bae694bcfdea775
Riley Smith <github@rileyy.dev>
2024-08-09 14:56:24 -0700

    fix some issues with og generation

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,5 @@
 import type { PageLoad } from './$types';
 
-export const prerender = true;
 
 export const load: PageLoad = ({ url }) => {
 	return {
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-
+	build: {
+		rollupOptions: {
+			external: ['@resvg/resvg-js', 'css-tree']
+		},
+	},
+	optimizeDeps: {
+		exclude: ['@resvg/resvg-js']
+	},
 };
 
 export default config;
