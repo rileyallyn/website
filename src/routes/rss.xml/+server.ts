47033770ab9627609ae2968732776191c4412ff3
Riley Smith <github@rileyy.dev>
2024-08-11 00:05:01 -0700

    fix rss bugs

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -4,48 +4,50 @@ import type { PostMetadata } from '../../types';
 // Some code taken from https://kylenazario.com/blog/full-content-rss-feed-with-sveltekit-part-two
 
 async function getRssXml(url: string) {
-	let rssUrl = url + '/rss.xml';
-	const root = create({ version: '1.0', encoding: 'UTF-8' })
-		.ele('feed', {
-			xmlns: 'http://www.w3.org/2005/Atom',
-		})
+	url = url.replace(/^http:/, 'https:');
+	const rssUrl = url + '/rss.xml';
+	const root = create({ version: '1.0', encoding: 'UTF-8', })
+		.ele('rss', {
+			version: '2.0',
+			'xmlns:atom': 'http://www.w3.org/2005/Atom',
+		}).ele('channel')
 		.ele('title').txt(`Riley Smith's Blog`).up()
-		.ele('link', { href: rssUrl }).up()
-		.ele('updated').txt(new Date().toISOString()).up()
-		.ele('id').txt(rssUrl).up()
-		.ele('author')
-		.ele('name').txt('Riley Smith').up()
-		.ele('email').txt('riley@rileysmith.me').up()
-		.up()
-		.ele('subtitle').txt('Writing about random things I find interesting.').up();
+		.ele('lastBuildDate').txt(new Date().toUTCString()).up()
+		.ele('link').txt(rssUrl).up()
+		.ele('managingEditor').txt('riley@rileysmith.me (Riley Smith)').up()
+		.ele('description').txt('Writing about random things I find interesting.').up()
+		.ele('atom:link', { rel: 'self', href: rssUrl })
+		.up();
 	const mdModules = import.meta.glob('../../posts/**/index.md');
 	const posts = await Promise.all(
 		Object.keys(mdModules).map(async (path) => {
 			const slug = path.split('/').at(-2);
 			const { metadata } = await mdModules[path]() as { metadata: PostMetadata };
-			const { datePublished, lastUpdated, title, description, locked } = metadata;
+			const { datePublished, title, description, locked } = metadata;
 			if (locked) {
 				return null;
 			}
-			return { datePublished, lastUpdated, title, description, slug };
+			return { datePublished, title, description, slug };
 		}),
 	);
 	if (posts.length === 0 || posts.every((post) => post === null)) {
-		return root.end();
+		return root.up().end();
 	}
 
 	for (const post of posts) {
 		const postUrl = url + '/blog/' + post!.slug;
-		root.ele('entry')
+		const postDate = new Date(post!.datePublished);
+
+		root.ele('item')
 			.ele('title').txt(post!.title).up()
-			.ele('link', { href: postUrl }).up()
-			.ele('updated').txt(post!.lastUpdated).up()
-			.ele('id').txt(postUrl).up()
-			.ele('summary').txt(post!.description).up()
+			.ele('pubDate').txt(postDate.toUTCString()).up()
+			.ele('link').txt(postUrl).up()
+			.ele('guid').txt(postUrl).up()
+			.ele('description').txt(post!.description).up()
 			.up();
 	}
 
-	return root.end();
+	return root.up().end();
 
 }
 
@@ -55,7 +57,8 @@ export async function GET(request: Request) {
 	return new Response(xml, {
 		headers: {
 			'Cache-Control': 'max-age=0, s-maxage=3600',
-			'Content-Type': 'application/rss+xml'
+			'Content-Type': 'application/xml',
+			'Content-Length': xml.length.toString()
 		},
 		status: 200
 	});
