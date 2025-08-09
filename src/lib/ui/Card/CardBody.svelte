4ea84cf709bb8e4f6453e15d5d9fb5c9c3a8159b
Riley Smith <github@rileyy.dev>
2025-08-09 16:28:02 -0700

    make cards look nicer

--- src/lib/ui/Card/CardBody.svelte
+++ src/lib/ui/Card/CardBody.svelte
@@ -10,7 +10,7 @@
 	const styles = {
 		unstyled: 'h-96 w-96 transform-3d  *:transform-3d',
 		'frosted-glass':
-			'h-96 w-96 transform-3d  *:transform-3d bg-neutral-200/5 dark:bg-white/5 shadow-md dark:shadow-none drop-shadow-lg  backdrop-blur-xs hover:bg-neutral-200/15 dark:hover:bg-white/10 border-2 border-purple-300/30 transition-all duration-300'
+			'h-96 w-96 transform-3d  *:transform-3d dark:bg-white/5 shadow-md dark:shadow-none drop-shadow-lg backdrop-blur-xs hover:bg-neutral-200/15 dark:hover:bg-white/10 border-2 border-purple-300/30 transition-all duration-300'
 	};
 
 	let { className, style = 'frosted-glass', children }: Props = $props();
