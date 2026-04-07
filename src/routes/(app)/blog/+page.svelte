a1032e57ea49b4ea4c71a89877e21fe09e520d33
Riley Smith <github@rileyy.dev>
2026-04-07 09:51:39 -0700

    more fixes

--- src/routes/(app)/blog/+page.svelte
+++ src/routes/(app)/blog/+page.svelte
@@ -10,10 +10,10 @@
 <Container>
 	<h1 class="max-w-fit border-b-2 border-purple-700 text-xl leading-relaxed font-bold">Blog</h1>
 	<div class="flex flex-col gap-y-10">
-			{#each posts as post, index (post?.slug ?? index)}
-				{#if post}
-					<BlogPost {post} />
-					{/if}
-			{/each}
+		{#each posts as post, index (post?.slug ?? index)}
+			{#if post}
+				<BlogPost {post} />
+			{/if}
+		{/each}
 	</div>
 </Container>
